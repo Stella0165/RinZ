c1604f41fd39f80731b6226fcc8d9507aafd3bc0
Stella Wong Kai Ning <swon0163@student.monash.edu>
2026-07-15 13:03:33 +0800

    Modified server js to matches file folder path

--- career-agent/server.js
+++ career-agent/server.js
@@ -16,6 +16,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 
+app.use(express.static("public"));
+
 function route(handler) {
   return async (req, res) => {
     try {
